90607af677b721e9f8f8ca74ce2fce103e93398a
Yaswanth <pyaswanth4@gmail.com>
2024-02-01 15:15:44 +0530

    scrambled to-do list

---
+++ src/App.js
@@ -0,0 +1,68 @@
+import Header from './Header';
+import SearchItem from './SearchItem';
+import AddItem from './AddItem';
+import Content from './Content';
+import Footer from './Footer';
+import { useState } from 'react';
+
+function App() {
+
+const [items, setItems] = useState(JSON.parse(localStorage.getItem('shoppinglist')))
+
+const [newItem, setNewItem] = useState('');
+const [search, setSearch] = useState('');
+
+const setAndSaveItems = (newItems) =>{
+  setItems(newItems);
+  localStorage.setItem('shoppinglist',JSON.stringify(newItems));
+}
+
+const addItem = (item) => {
+  const id = items.length ? items[items.length-1].id + 1 : 1;
+  const myNewItem = {id,checked:false, item};
+  const listItems = [...items, myNewItem];
+  setAndSaveItems(listItems);
+  }
+
+const handleCheck = (id) => {
+  const itemsList = items.map((item)=> item.id === id ? {...item,
+      checked: !item.checked }: item)
+      setAndSaveItems(itemsList);
+}
+
+const handleDelete = (id) => {
+  const itemsList = items.filter((item)=>(item.id !== id));
+  setAndSaveItems(itemsList);
+}
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  if(!newItem) return;
+  addItem(newItem);
+  setNewItem('');
+}
+
+  return (
+    <div className="App">
+      <Header title = 'Grocery List'></Header>
+      <AddItem
+        newItem = {newItem}
+        setNewItem = {setNewItem}
+        handleSubmit = {handleSubmit}
+      ></AddItem>
+      <SearchItem 
+        search = {search}
+        setSearch = {setSearch}
+      />
+      <Content 
+        items = {items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()))}
+        handleCheck = {handleCheck}
+        handleDelete = {handleDelete}
+      ></Content>
+
+      <Footer length = {items.length}/>
+    </div>
+  );
+}
+
+export default App;
